55fce11bf3b4217779e331d85e4cabf7a9707b3c
shrestha <shresthachakrabortykgp@gmail.com>
2025-12-19 12:11:00 +0530

    link correction

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://examify-ecru.vercel.app/",
+      "https://examify-ecru.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
